b230b64ed1916688496ad302284dd51013b5ae6d
Brigit Trujillo <trujillofloresbrigit@gmail.com>
2023-11-10 21:13:56 -0500

    Agregar archivos de proyecto.

---
+++ Lab11/Models/Detail.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab11.Models
+{
+    public class Detail
+    {
+        public int DetailId { get;set;}
+        public float Price { get;set;}
+        public float SubTotal { get;set;}
+
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int InvoiceId { get; set; }
+        public virtual Invoice Invoice { get; set; }
+
+
+    }
+}
---
+++ Lab11/Models/Invoice.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab11.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public DateTime Date { get; set;}
+        public string InvoiceNumber { get; set; }
+
+        public float Total { get; set;}
+
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
